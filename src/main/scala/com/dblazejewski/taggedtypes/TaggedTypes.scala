124de1edab67bd3050189422134f83d1732632c5
damian <damian@withlocals.com>
2018-01-10 23:33:55 +0100

    tagged types

--- src/main/scala/com/dblazejewski/taggedtypes/TaggedTypes.scala
+++ src/main/scala/com/dblazejewski/taggedtypes/TaggedTypes.scala
@@ -2,16 +2,45 @@ package com.dblazejewski.taggedtypes
 
 import java.time.LocalDate
 
-case class Booking(id: String, date: LocalDate)
+import com.dblazejewski.taggedtypes.tags.{BookingId, PaymentId}
+import shapeless.tag.@@
+import shapeless.tag
 
-case class Payment(id: String, bookingId: String, date: LocalDate)
+trait BookingIdTag
+
+trait PaymentIdTag
+
+package object tags {
+  type BookingId = String @@ BookingIdTag
+  type PaymentId = String @@ PaymentIdTag
+}
+
+case class Booking(id: BookingId, date: LocalDate)
+
+case class Payment(id: PaymentId, bookingId: BookingId, date: LocalDate)
 
 object TaggedTypes {
-  val booking1 = Booking("bookingId1", LocalDate.now)
-  val booking2 = Booking("bookingId2", LocalDate.now)
+  val bookingId1: BookingId = tag[BookingIdTag][String]("bookingId1")
+  val bookingId2: BookingId = tag[BookingIdTag][String]("bookingId2")
 
-  val payment1 = Payment("paymentId1", booking1.id, LocalDate.now)
-  val payment2 = Payment("paymentId2", booking2.id, LocalDate.now)
 
-  val bookingPaymentMapping: Map[String, String] = Map(booking1.id -> payment1.id, booking2.id -> payment2.id)
+  val paymentId: PaymentId = tag[PaymentIdTag][String]("paymentId")
+  val paymentId1: PaymentId = tag[PaymentIdTag][String]("paymentId1")
+  val paymentId2: PaymentId = tag[PaymentIdTag][String]("paymentId2")
+
+
+  val booking1 = Booking(bookingId1, LocalDate.now)
+  val booking2 = Booking(bookingId1, LocalDate.now)
+
+  val payment1 = Payment(paymentId1, booking1.id, LocalDate.now)
+  val payment2 = Payment(paymentId2, booking2.id, LocalDate.now)
+
+  val bookingPaymentMapping: Map[BookingId, PaymentId] = Map(booking1.id -> payment1.id, booking2.id -> payment2.id)
+}
+
+object Payments {
+
+  import TaggedTypes._
+
+  def payBooking(bookingId: BookingId) = Payment(paymentId, bookingId, LocalDate.now)
 }
